d4dae964a60eb5d3d105741be28c2c9bc781b221
ahnaf4d <ahnaftahmid802@gmail.com>
2024-04-24 10:00:40 +0600

    server setup

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const cors = require('cors');
+const port = process.env.PORT || 5000;
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.get('/', (req, res) => {
+  res.send('User Management Server Here!!!');
+});
+app.listen(port, () => {
+  console.log(`server running on ${port}`);
+});
---
+++ readme.md
@@ -0,0 +1,10 @@
+# User Management System Server
+
+This is crud operation practice project
+
+## Packages Used in these Project
+
+- express.js
+- mongoDB
+- dotenv
+- cors
