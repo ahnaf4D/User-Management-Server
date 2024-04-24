4cc02fcf00ea4cd602a6f8fb25d38ada30a9f3bf
ahnaf4d <ahnaftahmid802@gmail.com>
2024-04-24 10:09:57 +0600

    connected server to mongoDB

--- index.js
+++ index.js
@@ -1,12 +1,34 @@
+require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const express = require('express');
 const cors = require('cors');
 const port = process.env.PORT || 5000;
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.get('/', (req, res) => {
-  res.send('User Management Server Here!!!');
+
+const uri = `mongodb+srv://${process.env.USER_NAME}:${process.env.USER_PASS}@cluster0.zrua0aj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
 });
+
+async function run() {
+  try {
+    app.get('/', (req, res) => {
+      res.send('User Management Server Here!!!');
+    });
+    console.log(
+      'Pinged your deployment. You successfully connected to MongoDB!'
+    );
+  } finally {
+  }
+}
+run().catch(console.dir);
+
 app.listen(port, () => {
   console.log(`server running on ${port}`);
 });
